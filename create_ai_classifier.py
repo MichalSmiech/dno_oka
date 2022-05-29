65301accda5bcebc1ff811666f7916598bb110da
Michał Śmiech <michal.smiech@mlabs.pl>
2022-05-29 23:42:39 +0200

    clean

--- create_ai_classifier.py
+++ create_ai_classifier.py
@@ -1,12 +1,7 @@
-from simple_detector import SimpleDetector
 from ai_detector import AiDetector
-from datetime import datetime
 
-def ai():
+if __name__ == '__main__':
     detector = AiDetector()
 
     detector.create_classifier()
     detector.save_classifier()
-
-if __name__ == '__main__':
-    ai()
